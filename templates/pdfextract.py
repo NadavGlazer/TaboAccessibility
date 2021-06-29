6cf84ab87cbfa99af9cb78677fa8faf3f1ffec93
Nadav <nadav28223@gmail.com>
2021-06-29 19:33:51 +0300

    New algoritem of finding company names

--- templates/pdfextract.py
+++ templates/pdfextract.py
@@ -91,20 +91,17 @@ def line_information_extractor(info, type_of_file, sheet, people_row_count, comp
             print(info)
 
             if type_of_file == 1:
-                id_value= get_ID_from_sentence(info)
-               
+                id_value= get_ID_from_sentence(info)               
                 id_value = id_value.replace(" ", "")
 
                 sheet.cell(row=people_row_count, column=1).value = id_value
-                sheet.cell(row=people_row_count, column=1).font = Font(size=11, bold=False)
-    
+                sheet.cell(row=people_row_count, column=1).font = Font(size=11, bold=False)  
 
 
-                # Find the name and putting it in the excel by certain distance from the ID and the reason
-                # name_value = info[info.find(json_data['hebrew_ID']) + 3:info.find(
-                #     json_data['hebrew_ID']) + find_name_shared_homes(info)][::-1]
+                # Findind the name and putting it in the excel by certain distance from the ID and the reason               
                 name_value = get_ID_name_from_sentence(info)[::-1]                   
-                
+                name_value = name_value.replace("  ", " ")
+
                 sheet.cell(row=people_row_count, column=2).value = name_value
                 sheet.cell(row=people_row_count, column=2).font = Font(size=11, bold=False)
 
@@ -113,21 +110,17 @@ def line_information_extractor(info, type_of_file, sheet, people_row_count, comp
                 print(name_value)
 
             if type_of_file == 2:
-                # Find the ID and putting it in the excel (ID is always in the start of the file, very simple check)
-                #id_value = info[:info.find(json_data['hebrew_ID'])]     
-                id_value= id_value= get_ID_from_sentence(info)
-                        
+                # Find the ID 
+                id_value= id_value= get_ID_from_sentence(info)                        
                 id_value = id_value.replace(" ", "")
                 
                 sheet.cell(row=people_row_count, column=1,).value = id_value
                 sheet.cell(row=people_row_count, column=1).font = Font(size=11, bold=False)
 
-                # Find the name and putting it in the excel by certain distance from the ID and the reason
-                # name_value = info[info.find(json_data['hebrew_ID']) + 3:info.find(
-                #     json_data['hebrew_ID']) + find_name_shared_rights(info)][::-1]   
-                #       
+                # Finding the name 
                 name_value = get_ID_name_from_sentence(info)                    
-
+                name_value = name_value.replace("  ", " ")
+                
                 sheet.cell(row=people_row_count, column=2).value = name_value
                 sheet.cell(row=people_row_count, column=2).font = Font(size=11, bold=False)
 
@@ -147,16 +140,16 @@ def line_information_extractor(info, type_of_file, sheet, people_row_count, comp
             print(info)
 
             if type_of_file == 1:
-                # Find the company ID and putting it in the excel               
+                # Finding the company ID and putting it in the excel               
                 company_id_value= get_ID_from_sentence(info)               
                 company_id_value = company_id_value.replace(" ", "")
 
                 sheet.cell(row=company_row_count, column=5).value = company_id_value
                 sheet.cell(row=company_row_count, column=5).font = Font(size=11, bold=False)
 
-                # Find the name and putting it in the excel by certain distance from the ID and the reason
-                company_name_value = info[info.find(json_data['hebrew_Company']) + 4:info.find(
-                    json_data['hebrew_Company']) + find_company_name_shared_homes(info)][::-1]
+                # Finding the name             
+                company_name_value= get_company_name_from_sentence(info)
+
                 sheet.cell(row=company_row_count, column=6).value = company_name_value
                 sheet.cell(row=company_row_count, column=6).font = Font(size=11, bold=False)
 
@@ -165,16 +158,16 @@ def line_information_extractor(info, type_of_file, sheet, people_row_count, comp
                 print(company_id_value)
 
             if type_of_file == 2:
-                # Find the company ID and putting it in the excel             
+                # Finding the company ID and putting it in the excel             
                 company_id_value= get_ID_from_sentence(info)               
                 company_id_value = company_id_value.replace(" ", "")
                 
                 sheet.cell(row=company_row_count, column=5).value = company_id_value
                 sheet.cell(row=company_row_count, column=5).font = Font(size=11, bold=False)
 
-                # Find the name and putting it in the excel by certain distance from the ID and the reason
-                company_name_value = info[info.find(json_data['hebrew_Company']) + 4:info.find(
-                    json_data['hebrew_Company']) + find_company_name_shared_rights(info)][::-1]
+                # Finding the name             
+                company_name_value= get_company_name_from_sentence(info)
+
                 sheet.cell(row=company_row_count, column=6).value = company_name_value
                 sheet.cell(row=company_row_count, column=6).font = Font(size=11, bold=False)
 
@@ -187,7 +180,8 @@ def line_information_extractor(info, type_of_file, sheet, people_row_count, comp
 
             # Adding 1 to the index of where the program will write
             row_added = 2
-        # Checking if there`s passport and not mortgage in the line
+            
+        # Checking if there`s passport in the line
         elif json_data['hebrew_passport'] in info:
             info += " "
             info = " " + info
@@ -275,45 +269,6 @@ def write_file_type_in_excel(file_type, sheet):
     sheet.cell(row=2, column=11).font = Font(size=11, bold=False)
 
 
-def find_name_shared_rights(info):
-    """Returning the distance of the name from the ID"""
-    json_file = open(json_file_name, encoding="utf8")
-    json_data = json.load(json_file)
-
-    length = 3
-
-    info = info[info.find(json_data['hebrew_ID']) + 3:]
-
-    length += info.find(" ") + 1
-    info = info[info.find(" ") + 1:]
-
-    info = info[::-1]
-    print(info)
-    info = " ".join(info.split())
-    info = info[name_reason_filtering_shared_rights(info):]
-
-    info = " ".join(info.split())
-    print(info)
-    length += len(info)
-
-    return length
-
-
-def name_reason_filtering_shared_rights(info):
-    """Returning the string without the reason in order to find the name"""
-    json_file = open(json_file_name, encoding="utf8")
-    json_data = json.load(json_file)
-
-    index_of_reason = 0
-
-    for reason in json_data['possible_name_reasons']:
-        if reason in info:
-            index_of_reason = info.find(reason) + len(reason)
-            break
-    json_file.close()
-    return index_of_reason
-
-
 def name_reason_filtering_shared_homes(info):
     """Returning the string without the reason in order to find the name"""
 
@@ -327,28 +282,6 @@ def name_reason_filtering_shared_homes(info):
     json_file.close()
     return info
 
-
-def find_name_shared_homes(info):
-    """Returning the distance of the name from the ID"""
-    json_file = open(json_file_name, encoding="utf8")
-    json_data = json.load(json_file)
-
-    length = 3
-    info = info[info.find(json_data['hebrew_ID']) + 3:]
-
-    length += info.find(" ") + 1
-    info = info[info.find(" ") + 1:]
-
-    info = info[::-1]
-    print(info)
-
-    info = name_reason_filtering_shared_homes(info)
-
-    info = " ".join(info.split())
-    print(info)
-    length += len(info)
-
-    return length
 
 
 def find_passport_name_shared_homes(info):
@@ -378,58 +311,6 @@ def find_passport_name_shared_homes(info):
     return length
 
 
-def find_company_name_shared_homes(info):
-    """Returning the distance of the name from the company ID"""
-    json_file = open(json_file_name, encoding="utf8")
-    json_data = json.load(json_file)
-
-    length = 4
-    info = info[info.find(json_data['hebrew_Company']) + 4:]
-
-    length += info.find(" ") + 1
-    info = info[info.find(" ") + 1:]
-
-    info = info[::-1]
-    print(info)
-
-    for reason in json_data['possible_company_name_reasons']:
-        if reason in info:
-            info = info.replace(reason, "")
-    json_file.close()
-    info = " ".join(info.split())
-
-    print(info)
-    length += len(info)
-
-    return length
-
-
-def find_company_name_shared_rights(info):
-    """Returning the distance of the name from the company ID"""
-    json_file = open(json_file_name, encoding="utf8")
-    json_data = json.load(json_file)
-
-    length = 4
-    info = info[info.find(json_data['hebrew_Company']) + 4:]
-
-    length += info.find(" ") + 1
-    info = info[info.find(" ") + 1:]
-
-    info = info[::-1]
-    print(info)
-
-    for reason in json_data['possible_company_name_reasons']:
-        if reason in info:
-            info = info[:info.find(reason)]
-    json_file.close()
-    info = " ".join(info.split())
-
-    print(info)
-    length += len(info)
-
-    return length
-
-
 def find_file_type(info, sheet):
     """Returning the type of the file presented by numbers"""
     if "םיפתושמ םיתב" in info:
@@ -453,7 +334,7 @@ def get_ID_from_sentence(sentence):
 
 
 def get_ID_name_from_sentence(info):
-    """Returning the distance of the name from the ID"""
+    """Returning the name"""
     json_file = open(json_file_name, encoding="utf8")
     json_data = json.load(json_file)
 
@@ -486,7 +367,40 @@ def get_ID_name_from_sentence(info):
     info=info[::-1]
 
     return info
+
+
+def get_company_name_from_sentence(info):
+    json_file = open(json_file_name, encoding="utf8")
+    json_data = json.load(json_file)
+
+    info = info.replace(json_data['hebrew_Company'],"")
+    info=info[::-1]
+
+    for reason in json_data['possible_company_name_reasons']:
+        if reason in info:
+            info=info.replace(reason,"")
+    
+    for word in info:
+        if(word.isdigit() or '/' in word):
+            info= info.replace(word, "")
+
+    for count in range(0, len(info)):
+        if(info[count]==')'):
+            info = info[:count] + '(' + info[count+1:]
+        elif(info[count]=='('):
+            info = info[:count] + ')' + info[count+1:]
+
    
+    info = info.replace("  ", "")
+    for a in range(0,2):
+        if(info[len(info)-1] ==' ' or info[len(info)-1] =='-'):
+            info = info[:-1]            
+        elif(info[0] == ' ' or info[0] == '-'):
+            info = info[1:]          
+       
+
+    return info
+
 #get_ID_name_from_sentence(" 17728/2013/1 1 / 2 300812641 ז.ת הנוי רואמ רכמ")
 #pdf_to_txt('352.pdf')
 #print(find_passport_name_shared_homes(info))
