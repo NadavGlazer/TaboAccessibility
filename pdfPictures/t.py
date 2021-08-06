affa5c4beb894eb791a6553324eaacf6ef191c59
Nadav <nadav28223@gmail.com>
2021-08-07 01:40:22 +0300

    check

---
+++ pdfPictures/t.py
@@ -0,0 +1,4 @@
+import pdfkit
+options = {"enable-local-file-access": None}
+
+pdfkit.from_file("C:/Users/Nadav1/TaboProject/TaboAccessibility/1__06_08_2021_23_08_17.html", "te.pdf",options=options)
